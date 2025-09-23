aa26db9268d889543ca7940be2cee874b3c49159
Ben Cyna <benjamincyna@gmail.com>
2025-09-23 18:24:35 -0400

    check if first works

--- dp1.c
+++ dp1.c
@@ -24,42 +24,56 @@ int main(int argc, char **argv)
   }
 
   long N = strtol(argv[1], NULL, 10);
-  long repititions = strtol(argv[2], NULL, 10);
+  long repetitions = strtol(argv[2], NULL, 10);
 
-  float *pA = malloc(sizeof(float) * N);
-  float *pB = malloc(sizeof(float) * N);
+  float *pA = malloc(sizeof(float) * (size_t)N);
+  float *pB = malloc(sizeof(float) * (size_t)N);
+
+  if (pA == NULL || pB == NULL)
+  {
+    fprintf(stderr, "Error allocating memory: %s\n", strerror(errno));
+    free(pA);
+    free(pB);
+    return 1;
+  }
 
   // fill values of pA and pB
-  for (int k = 0; k < N; k++)
+  for (long k = 0; k < N; k++)
   {
     pA[k] = 1.0f;
     pB[k] = 1.0f;
   }
 
   struct timespec t0, t1;
-  long halfOfRepititions = repititions/2;
-  double* allProgramLengthTimes= malloc(sizeof(double) * halfOfRepititions); 
+  long halfOfRepetitions = repetitions/2;
+  double* allProgramLengthTimes= malloc(sizeof(double) * halfOfRepetitions); 
 
-  for (long i = 0; i < repititions; i++)
+  float res = 0.0f;
+  for (long i = 0; i < repetitions; i++)
   {
     clock_gettime(CLOCK_MONOTONIC, &t0);
-    float res = dp((long)N, pA, pB);
+    res = dp((long)N, pA, pB);
     clock_gettime(CLOCK_MONOTONIC, &t1);
-    if (i >= halfOfRepititions)
+    if (i >= halfOfRepetitions)
     { 
-        allProgramLengthTimes[i-halfOfRepititions] = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) * 1e-9;
+        allProgramLengthTimes[i-halfOfRepetitions] = ((double)t1.tv_sec - (double)t0.tv_sec) + ((double)t1.tv_nsec - (double)t0.tv_nsec) * 1e-9;
     }
-    fprintf(stderr, "Result: %f\n", res);
   }
+  fprintf(stderr, "Result: %f\n", res);
 
   double totalTime = 0.0;
-  for (long i = 0; i < halfOfRepititions; i++) {
+  for (long i = 0; i < halfOfRepetitions; i++) {
     totalTime += allProgramLengthTimes[i];
   }
-  double averageTime = totalTime / halfOfRepititions;
-  printf("N: %ld <T>: %f sec B: <TODO> GB/sec F: <TODO> FLOP/sec", N, averageTime);
+  double averageTime = totalTime / halfOfRepetitions;
+  double bytesPerCall = 2.0 * (double)sizeof(float) * (double)N;
+  double bandwidth = (bytesPerCall / 1e9) / averageTime;
+  double flops = (2.0 * (double)N) / averageTime;
+
+  printf("N: %ld <T>: %.6f sec B: %.3f GB/sec F: %.3f FLOP/sec\n", N, averageTime, bandwidth, flops);
 
   free(pA); 
   free(pB); 
   free(allProgramLengthTimes);
+  return 0;
 }
